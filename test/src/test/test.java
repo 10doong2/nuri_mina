1fbd1d1b65fd0f88988c96273270aa9e6728d072
wll1234 <wll27471297@gmail.com>
2017-01-24 16:53:44 +0900

    delete eeeeee

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -5,8 +5,6 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github");
-		System.out.println("eeeeeee");
-		System.out.println("eeeeeee");
 	}
 
 }
